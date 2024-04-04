21ad1e53e9b7c4f73c0a6dc897e26653beeccadb
Luca Holzer <luca.holzer@knapp.com>
2024-04-04 14:16:20 +0200

    added csrf token creation and verification

--- frontend.php
+++ frontend.php
@@ -1,8 +1,19 @@
 <?php
+session_start();
 include 'database.php';
+
+// Generate CSRF token
+if (empty($_SESSION['csrf_token'])) {
+    $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+}
 
 // Insert
 if (isset($_REQUEST['submit_insert'])) {
+    // Check CSRF token
+    if (!isset($_POST['csrf_token']) || !hash_equals($_SESSION['csrf_token'], $_POST['csrf_token'])) {
+        die('Invalid CSRF token');
+    }
+
     $stmt = database::dbConnection()->prepare("INSERT INTO user (name, password) VALUES (?, ?)");
     $stmt->bind_param("ss", $_REQUEST['Name'], $_REQUEST['Passwort']);
     if ($stmt->execute() === true) {
@@ -15,6 +26,11 @@ if (isset($_REQUEST['submit_insert'])) {
 
 // Login
 if (isset($_REQUEST['submit_login'])) {
+    // Check CSRF token
+    if (!isset($_POST['csrf_token']) || !hash_equals($_SESSION['csrf_token'], $_POST['csrf_token'])) {
+        die('Invalid CSRF token');
+    }
+
     $stmt = database::dbConnection()->prepare("SELECT * FROM `user` WHERE (password = ? AND name = ?)");
     $stmt->bind_param("ss", $_REQUEST['Passwort'], $_REQUEST['Name']);
     $stmt->execute();
@@ -47,14 +63,16 @@ database::dbConnection()->close();
 <body>
     <h1>Test-Seite SQL Injections</h1>
     <h2>Insert</h2>
-    <form action="frontend.php" method="get">
+    <form action="frontend.php" method="post">
+        <input type="hidden" name="csrf_token" value="<?= $_SESSION['csrf_token'] ?>">
         <input type="text" name="Name" placeholder="Name" required><br>
         <input type="password" name="Passwort" placeholder="Passwort" required><br>
         <input type="submit" name="submit_insert" value="Insert">
     </form>
     <hr>
     <h2>Login</h2>
-    <form action="frontend.php" method="get">
+    <form action="frontend.php" method="post">
+        <input type="hidden" name="csrf_token" value="<?= $_SESSION['csrf_token'] ?>">
         <input type="text" name="Name" placeholder="Name" required><br>
         <input type="password" name="Passwort" placeholder="Passwort" required><br>
         <input type="submit" name="submit_login" value="Login">
